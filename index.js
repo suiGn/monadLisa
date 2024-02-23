8807d403238da7032612378d4f1f002fc8e44050
suiGn <jabellae@gmail.com>
2024-02-23 07:13:47 +0000

    update monadLisa project

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
+import 'dotenv/config';
+import { createPool } from 'v.path'; // Adjust the path based on your project structure
 import Me from "this.me";
 import cleaker from "cleaker";
 //start express_server from server.cjs
 import { Gateway } from 'netget';
+//Plug in to LisaMemory
+const dbPool = createPool(
+  process.env.DB_HOST,
+  parseInt(process.env.DB_PORT, 10),
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  process.env.DB_NAME
+);
+
 // Configure your gateway instance
 const gateway = new Gateway({
-  port: 3000,
-  domainsConfigPath: './config/domains.json'
+  port: process.env.GATEWAY_PORT,
+  domainsConfigPath: process.env.DOMAINS_CONFIG_PATH
 });
-// Start the gateway
 gateway.listen();
