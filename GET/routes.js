f57de7f92e0dfd892143772d29a9fea82f20f0a1
Sui Gn <natsynapses@gmail.com>
2024-03-22 18:13:43 -0500

    update routes.js
    default handlers for routes.

--- GET/routes.js
+++ GET/routes.js
@@ -1,9 +1,12 @@
 // routes.js
 import { exampleRouteHandler } from './handlers/exampleRouteHandler.js';
 import { anotherDomainRouteHandler } from './handlers/anotherDomainRouteHandler.js';
+import { defaultHandler } from 'netget';  // Adjust the import path based on your structure
+
 
 export const routes = {
   'example.com': exampleRouteHandler,
+  '*.example.com': exampleRouteHandler,  // Will handle any subdomain of example.com
   'anotherdomain.com': anotherDomainRouteHandler,
-  'localhost':  exampleRouteHandler,
+  'localhost':  defaultHandler
 };
